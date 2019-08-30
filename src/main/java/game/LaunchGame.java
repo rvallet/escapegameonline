d653e1a1eaafdad382c29e6554b08e5f4ee7a548
Rémy VALLET <remy.vallet@worldline.com>
2019-08-30 12:22:12 +0200

    init maven project Excape Game from Gameplay Studio

---
+++ src/main/java/game/LaunchGame.java
@@ -0,0 +1,12 @@
+package game;
+
+public class LaunchGame {
+
+    public static void main(String[] args) {
+        start();
+    }
+
+    public static void start(){
+        System.out.println("You're not prepared !!!");
+    }
+}

ac38732a154d8696ed3e2156857f1aa6d1c56626
Rémy VALLET <remy.vallet@worldline.com>
2019-09-06 16:33:25 +0200

    advancement in the game s launch and properties files

--- src/main/java/game/LaunchGame.java
+++ src/main/java/game/LaunchGame.java
@@ -1,18 +1,20 @@
 package main.java.game;
 
 import main.java.utils.PropertiesReader;
+import main.java.utils.ScannerTools;
+import main.java.utils.TextAnimation;
 
 public class LaunchGame {
-/* static PropertiesReader pr = new PropertiesReader();*/
+ static PropertiesReader pr = new PropertiesReader();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("*** from Main class LaunchGame *** ");
         start();
 
         System.out.println("*** from Lauching ChallengerMode *** ");
         ChallengerMode cmGame = new ChallengerMode();
         System.out.println(cmGame.generateSecretNum());
-        System.err.println("Tentative : "+cmGame.getTentativeNum());
+ /*       System.err.println("Tentative : "+cmGame.getTentativeNum());
         System.out.println(cmGame.getMinArr());
         System.out.println(cmGame.getMaxArr());
         cmGame.adjustMinMax("-==+");
@@ -21,11 +23,42 @@ public class LaunchGame {
         System.out.println("Number : "+cmGame.getSecretNum());
         System.out.println(cmGame.generateSecretNum());
         System.err.println("Tentative : "+cmGame.getTentativeNum());
-        System.out.println("Number : "+cmGame.getSecretNum());
+        System.out.println("Number : "+cmGame.getSecretNum());*/
 
     }
 
-    public static void start(){
-        System.out.println("You're not prepared !!!");
+    public static void start() throws InterruptedException {
+        TextAnimation.gradualText(pr.getContent("content.intro1"));
+        TextAnimation.gradualText(pr.getContent("content.intro2"));
+        System.out.println("");
+        TextAnimation.gradualText(pr.getContent("content.intro3"));
+        TextAnimation.gradualText(pr.getContent("content.intro4"));
+        TextAnimation.gradualText(pr.getContent("content.intro5"));
+        System.out.println("");
+        System.out.println(pr.getContent("content.intro.mode1"));
+        System.out.println(pr.getContent("content.intro.mode2"));
+        System.out.println(pr.getContent("content.intro.mode3"));
+        System.out.println("");
+        System.out.println(pr.getContent("content.msg1"));
+        System.out.println(pr.getContent("content.msg1.deco0"));
+        System.out.println(pr.getContent("content.msg1.mode1"));
+        System.out.println(pr.getContent("content.msg1.mode2"));
+        System.out.println(pr.getContent("content.msg1.mode3"));
+        System.out.println(pr.getContent("content.msg1.deco0"));
+        System.out.print("> ");
+        switch(ScannerTools.readLine()){
+            case "1":
+                System.out.println("1");
+                break;
+            case"2":
+                System.out.println("2");
+                break;
+            case "3":
+                System.out.println("3");
+                break;
+            default:
+                //Todo : log4j
+                System.err.println("Vous devez entrer un choix parmis ceux proposés (chiffre : 1, 2 ou 3)");
+        };
     }
 }

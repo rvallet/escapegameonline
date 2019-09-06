8020f632a80c8b1530bb69618b2d4b2c5eece6aa
Rémy VALLET <remy.vallet@worldline.com>
2019-09-06 14:39:29 +0200

    challenger mode in progress

--- src/main/java/game/ChallengerMode.java
+++ src/main/java/game/ChallengerMode.java
@@ -2,6 +2,9 @@ package main.java.game;
 import main.java.utils.PropertiesReader;
 import main.java.utils.RanChoice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Search for an X number combination in Challenger mode :
  * The AI of the computer plays the role of defender. It defines a combination of X number randomly
@@ -18,28 +21,55 @@ public class ChallengerMode {
 
     /* Class variable */
     private String secretNum;
-    private int tentativeNum=0;
-    private int min=0;
-    private int max=9;
+    private int tentativeNum;
+/*    private int min=0;
+    private int max=9;*/
+    private List<Integer> minArr=new ArrayList<>();
+    private List<Integer> maxArr=new ArrayList<>();
 
     /* Tools :  PropertiesReader */
-    PropertiesReader pr = new PropertiesReader();
+    static PropertiesReader pr = new PropertiesReader();
 
     /* Class constructor */
-    public ChallengerMode(int nbDigit, int nbTries, Boolean devMode) {
-        this.nbDigit = nbDigit;
-        this.nbTries = nbTries;
-        this.devMode = devMode;
+    public ChallengerMode() {
+        this.nbDigit = pr.getIntProp("settings.nbDigit");
+        this.nbTries = pr.getIntProp("settings.nbTries");
+        this.devMode = pr.getBoolProp("settings.devMode");
+        this.tentativeNum=0;
+        for (int i=0; i<pr.getIntProp("settings.nbDigit"); i++) {
+            this.minArr.add(0);
+            this.maxArr.add(9);
+        }
     }
 
     public String generateSecretNum () {
         StringBuilder sb = new StringBuilder();
         for (int i=0; i<this.nbDigit; i++){
-            sb.append(RanChoice.ranChoice(this.min, this.max));
+            sb.append(RanChoice.ranChoice(this.minArr.get(i), this.maxArr.get(i)));
         }
         setSecretNum(sb.toString());
         setTentativeNum(getTentativeNum()+1);
     return sb.toString();
+    }
+
+    public void adjustMinMax (String userInput) {
+        for (int i=0; i<userInput.length(); i++) {
+            char temp = userInput.charAt(i);
+            switch (temp) {
+                case '+':
+                    this.minArr.set(i, Integer.parseInt(String.valueOf(getSecretNum().charAt(i)))+1);
+                    break;
+                case '-':
+                    this.maxArr.set(i, Integer.parseInt(String.valueOf(getSecretNum().charAt(i)))-1);
+                    break;
+                case '=':
+                    this.minArr.set(i, Integer.parseInt(String.valueOf(getSecretNum().charAt(i))));
+                    this.maxArr.set(i, Integer.parseInt(String.valueOf(getSecretNum().charAt(i))));
+                    break;
+                default:
+                    System.err.println("invalid input" + userInput);
+            }
+        }
     }
 
     /* Getter and Setter */
@@ -57,5 +87,21 @@ public class ChallengerMode {
 
     public void setTentativeNum(int tentativeNum) {
         this.tentativeNum = tentativeNum;
+    }
+
+    public List<Integer> getMinArr() {
+        return minArr;
+    }
+
+    public void setMinArr(List<Integer> minArr) {
+        this.minArr = minArr;
+    }
+
+    public List<Integer> getMaxArr() {
+        return maxArr;
+    }
+
+    public void setMaxArr(List<Integer> maxArr) {
+        this.maxArr = maxArr;
     }
 }
--- src/main/java/game/LaunchGame.java
+++ src/main/java/game/LaunchGame.java
@@ -3,18 +3,25 @@ package main.java.game;
 import main.java.utils.PropertiesReader;
 
 public class LaunchGame {
+/* static PropertiesReader pr = new PropertiesReader();*/
 
     public static void main(String[] args) {
         System.out.println("*** from Main class LaunchGame *** ");
         start();
-        PropertiesReader pr = new PropertiesReader();
-        System.out.println(pr.getIntProp("settings.nbDigit"));
-        System.out.println(pr.getIntProp("settings.nbTries"));
-        System.out.println(pr.getBoolProp("settings.devMode"));
 
         System.out.println("*** from Lauching ChallengerMode *** ");
-        ChallengerMode CMgame = new ChallengerMode(pr.getIntProp("settings.nbDigit"), pr.getIntProp("settings.nbTries"), pr.getBoolProp("settings.devMode"));
-        System.out.println(CMgame.generateSecretNum());
+        ChallengerMode cmGame = new ChallengerMode();
+        System.out.println(cmGame.generateSecretNum());
+        System.err.println("Tentative : "+cmGame.getTentativeNum());
+        System.out.println(cmGame.getMinArr());
+        System.out.println(cmGame.getMaxArr());
+        cmGame.adjustMinMax("-==+");
+        System.out.println("updatedMin : "+cmGame.getMinArr());
+        System.out.println("updatedMax : "+cmGame.getMaxArr());
+        System.out.println("Number : "+cmGame.getSecretNum());
+        System.out.println(cmGame.generateSecretNum());
+        System.err.println("Tentative : "+cmGame.getTentativeNum());
+        System.out.println("Number : "+cmGame.getSecretNum());
 
     }
 

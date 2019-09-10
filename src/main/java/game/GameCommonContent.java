b8ab18afc53c49503776be9340cd428fc0ffbc8e
Rémy VALLET <remy.vallet@gmail.com>
2019-09-10 23:41:41 +0200

    updated properties en threadsleep intro

--- src/main/java/game/GameCommonContent.java
+++ src/main/java/game/GameCommonContent.java
@@ -10,76 +10,98 @@ public class GameCommonContent {
 
     public static void gameIntroduction() throws InterruptedException {
         TextAnimation.gradualText(pr.getContent("content.intro1"));
+        Thread.sleep(500);
         TextAnimation.gradualText(pr.getContent("content.intro2"));
+        Thread.sleep(1000);
         System.out.println("");
         TextAnimation.gradualText(pr.getContent("content.intro3"));
+        Thread.sleep(1000);
         TextAnimation.gradualText(pr.getContent("content.intro4"));
+        Thread.sleep(1000);
         TextAnimation.gradualText(pr.getContent("content.intro5"));
+        Thread.sleep(1000);
         System.out.println(" ");
         System.out.println(pr.getContent("content.intro.mode1"));
+        Thread.sleep(500);
         System.out.println(pr.getContent("content.intro.mode2"));
+        Thread.sleep(500);
         System.out.println(pr.getContent("content.intro.mode3"));
+        Thread.sleep(2000);
         System.out.println(" ");
     }
     public static void gameStartMenu(){
-        System.out.println(pr.getContent("content.msg1"));
-        System.out.println(pr.getContent("content.msg1.deco0"));
-        System.out.println(pr.getContent("content.msg1.mode1"));
-        System.out.println(pr.getContent("content.msg1.mode2"));
-        System.out.println(pr.getContent("content.msg1.mode3"));
-        System.out.println(pr.getContent("content.msg1.deco0"));
+        System.out.println(pr.getContent("content.sm.msg1"));
+        System.out.println(pr.getContent("content.sm.msg1.deco0"));
+        System.out.println(pr.getContent("content.sm.msg1.mode1"));
+        System.out.println(pr.getContent("content.sm.msg1.mode2"));
+        System.out.println(pr.getContent("content.sm.msg1.mode3"));
+        System.out.println(pr.getContent("content.sm.msg1.deco0"));
 
-        System.out.print("Human> ");
-        switch (ScannerTools.readLine()) {
-            case "1":
-                playAgain=false;
-                do {
-                    new ChallengerMode().run();
-                    GameCommonContent.gameEndMenu();
-                } while (playAgain);
-                break;
-            case "2":
-                playAgain=false;
-                do {
-                    System.out.println("2");
-                } while (playAgain);
-                break;
-            case "3":
-                playAgain=false;
-                System.out.println("3");
-                break;
-            default:
-                //Todo : log4j
-                System.err.println("Vous devez entrer un choix parmis ceux proposés (chiffre : 1, 2 ou 3)");
-        }
+        Boolean isValidChoice;
+        do {
+            System.out.print(pr.getContent("content.sm.msg2"));
+            switch (ScannerTools.readLine()) {
+                case "1":
+                    playAgain = false;
+                    isValidChoice=true;
+                    do {
+                        new ChallengerMode().run();
+                        GameCommonContent.gameEndMenu();
+                    } while (playAgain);
+                    break;
+                case "2":
+                    playAgain = false;
+                    isValidChoice=true;
+                    do {
+                        System.out.println("2");
+                    } while (playAgain);
+                    break;
+                case "3":
+                    playAgain = false;
+                    isValidChoice=true;
+                    System.out.println("3");
+                    break;
+                default:
+                    isValidChoice=false;
+                    System.out.println(" ");
+                    System.err.println(pr.getContent("content.sm.msg3.error"));
+            }
+        } while (!isValidChoice);
     }
     public static void gameEndMenu(){
-        System.out.println("*******************    Menu     ******************");
-        System.out.println("1 - Rejouer une partie identique ?     (taper '1')");
-        System.out.println("2 - Choisir un autre mode de jeu ?     (taper '2')");
-        System.out.println("3 - Quitter l'application ?            (taper '3')");
-        System.out.println("**************************************************");
-        System.out.println(" ");
-        System.out.print("Human > ");
-        int choice = ScannerTools.readInt(3);
-        switch (choice) {
-            case 1:
-                System.out.println("*** Nouvelle partie ***");
-                playAgain=true;
-                break;
-            case 2:
-                System.out.println("*** Retour au menu ***");
-                playAgain=false;
-                gameStartMenu();
-                break;
-            case 3:
-                System.out.println("*** Quitter le programme ***");
-                playAgain=false;
-                System.exit(0);
-                break;
-            default:
-                System.out.println("Vous devez faire un choix parmis ceux proposés");
-                break;
-        }
+        System.out.println(pr.getContent("content.em.deco0"));
+        System.out.println(pr.getContent("content.em.msg1.mode1"));
+        System.out.println(pr.getContent("content.em.msg1.mode2"));
+        System.out.println(pr.getContent("content.em.msg1.mode3"));
+        System.out.println(pr.getContent("content.em.deco1"));
+
+        Boolean isValidChoice;
+        do {
+            System.out.print(pr.getContent("content.em.msg2"));
+            switch (ScannerTools.readLine()) {
+                case "1":
+                    isValidChoice=true;
+                    System.out.println(pr.getContent("content.em.msg4.mode1"));
+                    playAgain = true;
+                    break;
+                case "2":
+                    isValidChoice=true;
+                    System.out.println(pr.getContent("content.em.msg4.mode2"));
+                    playAgain = false;
+                    gameStartMenu();
+                    break;
+                case "3":
+                    isValidChoice=true;
+                    System.out.println(pr.getContent("content.em.msg4.mode3"));
+                    playAgain = false;
+                    System.exit(0);
+                    break;
+                default:
+                    isValidChoice=false;
+                    System.out.println();
+                    System.err.println(pr.getContent("content.em.msg4.error"));
+                    break;
+            }
+        } while (!isValidChoice);
     }
 }
